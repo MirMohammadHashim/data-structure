5df4ac61a869153083f398a34749b1002f9cacb5
Faheem Firdous <faheemfirdous77@gmail.com>
2023-10-01 18:37:05 +0530

    Update Stack using linked list.cpp

--- Stack using linked list.cpp
+++ Stack using linked list.cpp
@@ -1,68 +1,91 @@
+// Queue is an abstract linear data structure, it is an ordered listed which works on the principle of first in first out FIFO
+// here insertion would be done on the last node (rear or tail) we use term enqueue for insertion
+// and deletion would be done on first node (front or head) we use dequeue for delete operation
+
 #include <iostream>
 using namespace std;
 
-struct stack
+struct Queue
 {
     int data;
-    struct stack *previous;
+    struct Queue *next;
 };
 
-struct stack *TOP = NULL;
+struct Queue *FRONT = NULL;
+struct Queue *REAR = NULL;
 
-void push()
+void Enqueue()
 {
-    struct stack *newNode;                              // initialize
-    newNode = new (struct stack[sizeof(struct stack)]); // memory allocation
-    cout << "Enter data:--> " << endl;
-    cin >> newNode->data;
+    struct Queue *temp;
+    temp = new (struct Queue[sizeof(struct Queue)]);
+    cout << "Enter data: ";
+    cin >> temp->data;
 
-    if (TOP == NULL)
+    if (FRONT == NULL)
     {
-        TOP = newNode;
-        TOP->previous = NULL;
+        FRONT = temp;
+        REAR = temp;
     }
     else
     {
-        newNode->previous = TOP;
-        TOP = newNode;
+        REAR->next = temp;
+        REAR = temp;
+        temp->next = NULL;
     }
 }
-void pop() // delete and print top node
+
+void Dequeue()
 {
-    if (TOP == NULL)
+    if (FRONT == NULL)
     {
-        cout << "Stack is empty:(\nTry pushing some elements to it:)" << endl;
+        cout << "The Queue is empty:(\nTry enqueuing some elements" << endl;
     }
     else
     {
-        cout << "The deleting data at top which is " << TOP->data << endl;
-        TOP = TOP->previous;
+        struct Queue *temp = FRONT;
+        cout << "Dequeuing " << FRONT->data << endl;
+        FRONT = FRONT->next;
+        free(temp);
     }
 }
-void peak()
+
+void front()
 {
-    if (TOP == NULL)
+    if (FRONT == NULL)
     {
-        cout << "Stack is empty:(\nTry pushing some elements to it:)" << endl;
+        cout << "The Queue is empty:(\nTry enqueuing some elements" << endl;
     }
     else
     {
-        cout << "The data at top is " << TOP->data << endl;
+        cout << "The element at front is " << FRONT->data << endl;
     }
 }
+
+void isEmpty()
+{
+    if (FRONT == NULL)
+    {
+        cout << "The Queue is empty:(\nTry enqueuing some elements" << endl;
+    }
+    else
+    {
+        cout << "The Queue is not empty" << endl;
+    }
+}
+
 void display()
 {
-    struct stack *temp = TOP;
-    if (TOP == NULL)
+    if (FRONT == NULL)
     {
-        cout << "Stack is empty:(\nTry pushing some elements to it:)" << endl;
+        cout << "The Queue is empty:(\nTry enqueuing some elements" << endl;
     }
     else
     {
+        struct Queue *temp = FRONT;
         while (temp != NULL)
         {
-            cout << "The data is = " << temp->data << endl;
-            temp = temp->previous;
+            cout << "The data is--> " << temp->data << endl;
+            temp = temp->next;
         }
     }
 }
@@ -73,26 +96,30 @@ int main()
     while (choice != 0)
     {
         cout << "\nEnter choice--> " << endl
-             << "1 to push " << endl
-             << "2 to pop " << endl
+             << "1 to Enqueue " << endl
+             << "2 to Dequeue " << endl
              << "3 to peak " << endl
              << "4 to display " << endl
-             << "NULL to end " << endl;
+             << "5 to check is Queue is empty " << endl
+             << "0 to end " << endl;
         cin >> choice;
 
         switch (choice)
         {
         case 1:
-            push();
+            Enqueue();
             break;
         case 2:
-            pop();
+            Dequeue();
             break;
         case 3:
-            peak();
+            front();
             break;
         case 4:
             display();
+            break;
+        case 5:
+            isEmpty();
             break;
         case 0:
             cout << "Ending program bye";
